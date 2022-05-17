7e417448f184d3e3bd51acb5739bc46e59458eec
euhan sarkar <euhansarkar@gmail.com>
2022-05-17 12:24:54 +0600

    second commit

--- play.js
+++ play.js
@@ -4,6 +4,7 @@
 //Date: 16-05-2022
 
 //let catch the references
+/*
 let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
 let todoUl = document.querySelector('#items');
@@ -62,3 +63,64 @@ let bindCompleteItems = function(taskItem, deleteButtonClick){
 }
 
 form.addEventListener('submit', addTask)
+*/
+
+// catch the references
+let form = document.querySelector('form');
+let newTask = document.querySelector('#new-task');
+let incompleteTask = document.querySelector('#items');
+let completeTask = document.querySelector('.complete-list ul');
+
+//functions 
+
+let createTask = function(task){
+      let listItem = document.createElement('li');
+      let inputCheckbox = document.createElement('input');
+      let lebelText = document.createElement('label');
+
+      inputCheckbox.type = 'checkbox';
+      lebelText.innerText = task;
+
+      listItem.appendChild(inputCheckbox);
+      listItem.appendChild(lebelText);
+      return listItem;
+}
+
+let addTask = function(event){
+      event.preventDefault();
+      let listItem = createTask(newTask.value);
+      incompleteTask.appendChild(listItem);
+      newTask.value = '';
+      bindIncompleteItems(listItem, completeItems)
+}
+
+let completeItems = function(){
+      let listItem = this.parentNode;
+      let deleteBtn = document.createElement('button');
+      deleteBtn.className = 'delete';
+      deleteBtn.innerText = 'Delete';
+      listItem.appendChild(deleteBtn);
+
+      let checkbox = listItem.querySelector('input[type="checkbox"]');
+      checkbox.remove();
+      completeTask.appendChild(listItem);
+
+      bindCompleteItems(listItem, deleteItem);
+}
+
+let deleteItem = function(){
+      let listItem = this.parentNode;
+      listItem.remove();
+}
+
+let bindCompleteItems = function(listTask, deleteItemClick){
+      let deleteBtn = listTask.querySelector('.delete');
+      deleteBtn.onclick = deleteItemClick;
+}
+
+let bindIncompleteItems = function(taskLIst, checkboxClick){
+      let checkBox = taskLIst.querySelector('input[type="checkbox"]');
+      checkBox.onchange = checkboxClick;
+}
+
+form.addEventListener('submit', addTask);
